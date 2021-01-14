0788fadfc2ec8329f6ff91440b839a1881c25a3d
Vitor Nunes <vitornunes200@gmail.com>
2021-01-14 19:35:31 -0300

    Exercicio 2

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,16 +3,19 @@ import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
 
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.handleClick = this.handleClick.bind(this);
+        this.fireButton = this.fireButton.bind(this);
+        this.psyButton = this.psyButton.bind(this);
         this.state = {
-            pokemonObj: 0
+            pokemonObj: 0,
+            pokemonArray: this.props.pokemons
         }
     }
 
     handleClick = () => {
-        if (this.props.pokemons[((this.state.pokemonObj) + 1)] === undefined) {
+        if (this.state.pokemonArray[((this.state.pokemonObj) + 1)] === undefined) {
             this.setState((previosState, _props) => ({
                 pokemonObj: -1
             }));
@@ -22,15 +25,31 @@ class Pokedex extends React.Component {
         }));
     }
 
+    fireButton = () => {
+        this.setState((previosState, _props) => ({
+            pokemonArray: this.props.pokemons.filter((type) => type.type === 'Fire')
+        }));
+    }
+
+    psyButton = () => {
+        this.setState((previosState, _props) => ({
+            pokemonArray: this.props.pokemons.filter((type) => type.type === 'Psychic')
+        }));
+    }
+
     render() {
         return (
             // <div className="pokedex">
             //     {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
             // </div>
-            <div className="pokedex">
-                <Pokemon pokemon={this.props.pokemons[(this.state.pokemonObj)]} />
+            < div className="pokedex" >
+                <Pokemon pokemon={this.state.pokemonArray[(this.state.pokemonObj)]} />
+                <div>
+                    <button className="typeButton" onClick={this.fireButton}>Fire</button>
+                    <button className="typeButton" onClick={this.psyButton}>Psychic</button>
+                </div>
                 <button className="nextPokemon" onClick={this.handleClick}>Próximo Pokémon</button>
-            </div>
+            </div >
         );
     }
 }
